6a6d50571ff4cd0d87b6d132e61b24af050ab43d
Minkun00 <qww9363a@gmail.com>
2023-08-03 13:11:57 +0900

    주석 처리

--- src/contracts/SetTeam.sol
+++ src/contracts/SetTeam.sol
@@ -3,14 +3,16 @@ pragma solidity >=0.4.22<0.9.0;
 import './Token.sol';
 
 contract SetTeam is Token {
+    // 팀 관련 변수 접근은 모두 mapping으로 들어감
+    // stringToBytes32(<Team Name>) 을 mapping에 넣어야 해당 값이 튀어나오게 했음.
+
+    // team weight에 접근하는 mapping
     mapping(bytes32 => uint) public teamWeight;
+    // team이 입력되었는지 확인용으로 만든 mapping
     mapping(bytes32 => bool) public teamSetted;
 
     uint bettingTotalBalance = 0;
     uint winnerTeamPureBalance = 0;
-
-    bytes32 team1;
-    bytes2 team2;
 
     modifier onlyOwner() {
         require(msg.sender == owner);
@@ -27,6 +29,7 @@ contract SetTeam is Token {
         }
     }
     
+    // team 설정
     function setTeam(string calldata _teamName) external onlyOwner {
         require(bytes(_teamName).length > 0, "Team name cannot be empty");
         bytes32 teamNameHash = stringToBytes32((_teamName));
@@ -35,6 +38,7 @@ contract SetTeam is Token {
         teamSetted[teamNameHash] = true;
     }
 
+    // team weight를 불러오기
     function getTeamWeightByTeamName(string calldata _teamName) external view returns (uint) {
         return teamWeight[stringToBytes32(_teamName)];
     }
--- src/contracts/Vote.sol
+++ src/contracts/Vote.sol
@@ -4,29 +4,35 @@ import './SetTeam.sol';
 // Vote.sol
 
 contract Vote is SetTeam {
-    bytes32 team1;
+    // vote.sol에서 function versus( )에서 정해지는 두 팀
+    // 이 두 팀의 게임에 대한 결과에 따라 betting result 과정을 거친다
+    bytes32 team1;  
     bytes32 team2;
+    // 이긴 팀의 이름을 저장, default : 'none'
     bytes32 teamNameWin = stringToBytes32('none');
+    // versus과정 이후에 모든 과정들을 실행할 수 있게 modifier 작성했는데, 그걸 위한 bool 변수
+    // versus 전 : false , versus 후 : true , returnBettingResultOver() 실행되면 다시 false...
     bool public versusExecuted = false;
-
+    // returnBettingResult()에서 returnBettingResultOver가 자동 실행될 경우를 대비해서
+    // react에서 인식하게 하기 위함.(추가적 returnBettingResultOver 실행 방지용)
     event BettingResultOver();
-
+    // versus 실행 전
     modifier onlyBeforeVersus() {
         require(!versusExecuted, "Versus has already been executed");
         _;
     }
-
+    // versus 실행 후
     modifier onlyAfterVersus() {
         require(versusExecuted, "Versus has not been executed yet");
         _;
     }
-
+    // team1, team2 저장
     function versus(string memory _team1, string memory _team2) public onlyBeforeVersus {
         team1 = stringToBytes32(_team1);
         team2 = stringToBytes32(_team2);
         versusExecuted = true;
     }
-
+    // vote 과정 (전과 동일)
     function vote(string memory _teamName, uint _amount) public onlyAfterVersus returns (bool) {
         require(!userData[msg.sender].voted, "You have already voted!");
         require(balanceOf(msg.sender) >= _amount, "Not enough tokens!");
@@ -44,7 +50,7 @@ contract Vote is SetTeam {
 
         return true;
     }
-
+    // game end : 전과 동일
     function gameEnd(string memory _teamNameWin, string memory _teamNameLose) public onlyAfterVersus onlyOwner {
         bytes32 win = stringToBytes32(_teamNameWin);
         bytes32 lose = stringToBytes32(_teamNameLose);
@@ -56,7 +62,8 @@ contract Vote is SetTeam {
         bettingTotalBalance = teamWeight[win];
         teamWeight[lose] = 0;
     }
-
+    // 추가된 점...
+    // 이상적으로 모든 user들이 돈을 받아갔으면 retrunBettingResultOver()자동 실행
     function returnBettingResult() public onlyAfterVersus {
         require(teamNameWin != stringToBytes32('none'));
 
@@ -73,7 +80,7 @@ contract Vote is SetTeam {
         userData[msg.sender].voteBalance = 0;
         userData[msg.sender].voted = false;
     }
-
+    // 이 함수가 실행되어야지 다시 versus()함수를 실행 가능함
     function returnBettingResultOver() public onlyOwner onlyAfterVersus {
         require(teamNameWin != stringToBytes32('none'));
         totalSupply += teamWeight[teamNameWin];
