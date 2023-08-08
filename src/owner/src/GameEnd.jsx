975f676d8e71db2cc3ac32cea61d3af4b756d6bb
HangMin Cho <slightsmille@gmail.com>
2023-08-08 20:32:51 +0900

    gameEnd Button

--- src/owner/src/GameEnd.jsx
+++ src/owner/src/GameEnd.jsx
@@ -3,16 +3,44 @@ import React from "react"
 import { gameEnd_ } from "../function/Ownerfunction"
 
 class GameEnd extends React.Component {
+    
+
     handleGameEnd = async() => {
-        const { contract, homeTeam, awayTeam, account } = this.props
-        await gameEnd_(contract, homeTeam, awayTeam, account)
-        console.log(`Win : ${homeTeam}, Lose : ${awayTeam}`)
+        const { contract, account } = this.props
+        const { winnerTeam, loserTeam } = this.state;
+
+        await gameEnd_(contract, winnerTeam, loserTeam, account)
+        console.log(`Win : ${winnerTeam}, Lose : ${loserTeam}`)
+    }
+
+    setWinnerLoserTeam = (_winnerTeam, _loserTeam) => {
+        this.setState({
+            winnerTeam: _winnerTeam,
+            loserTeam: _loserTeam
+        });
+    }
+
+    constructor() {
+        super()
+        this.state ={
+            winnerTeam: null,
+            loserTeam: null
+        }
+        
     }
     
     render() {
+        const { homeTeam, awayTeam } = this.props
+
         return(
             <div>
-                <button onClick={this.handleGameEnd}>GAME END</button>
+                <div>
+                    <button onClick={() => this.setWinnerLoserTeam(homeTeam, awayTeam)}> {homeTeam} </button>
+                    <button onClick={() => this.setWinnerLoserTeam(awayTeam, homeTeam)}> {awayTeam} </button>
+                </div>
+                <div>
+                    <button onClick={this.handleGameEnd}>GAME END</button>
+                </div>
             </div>
         )
     }
